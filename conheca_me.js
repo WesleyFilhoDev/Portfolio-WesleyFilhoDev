d091b34c8b8d86da005ee8f8d4e69da9d8a700d6
WesleyFilhoDev <wesleyfilho221204@gmail.com>
2024-11-16 12:05:48 -0300

    responsividde quase 100%

--- conheca_me.js
+++ conheca_me.js
@@ -13,6 +13,9 @@ const healnet = document.getElementById('healnet');
 const formulario = document.getElementById('formulario');
 const jogo_senha = document.getElementById('jogo_senha');
 const contato = document.getElementById('contato');
+const btn_abrir_menu = document.getElementById('btn_abrir_menu');
+const menu_mobile = document.getElementById('menu_mobile');
+const overlay_menu = document.getElementById('overlay_menu');
 
 buttom_mode.addEventListener('click', () =>{
     buttom_mode.classList.toggle('dark');
@@ -29,5 +32,17 @@ buttom_mode.addEventListener('click', () =>{
     formulario.classList.toggle('dark');
     jogo_senha.classList.toggle('dark');
     contato.classList.toggle('dark');
+});
+
+btn_abrir_menu.addEventListener('click', () =>{
+    menu_mobile.classList.add('abrir_menu')
+});
+
+menu_mobile.addEventListener('click', () =>{
+    menu_mobile.classList.remove('abrir_menu')
+});
+
+overlay_menu.addEventListener('click', () =>{
+    menu_mobile.classList.remove('abrir_menu')
 });
 
